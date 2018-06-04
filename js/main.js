7ecdb7afcb36e9268cf1fe6eef38cf5ee0e09137
vintosaweb <vintos92@mail.ru>
2018-06-05 00:47:07 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,65 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() >= 2450) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function() {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop : 0                       // Scroll to top of body
+    }, 500);
+});
+
+
+$('document').ready(function() {
+    $('.phone-mask').mask("+375 (99) 999-99-99");
+});
+
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 75)
+        }, 1000, "easeOutCubic");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 96
+  });
+
+})(jQuery); // End of use strict
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+	
